4dee4a6baaf9c19383bbabf0c7304a8b183f212f
Nathan Preston <nathanpreston@aol.com>
2019-11-22 13:30:38 +0000

    All items

---
+++ FireBoots.java
@@ -0,0 +1,14 @@
+/**
+ * Class of the FireBoots that the user picks up to be able to walk through fire
+ * @author Nathan
+ * @version 1.0
+ */
+public class FireBoots extends Item {
+
+	/**
+	 * @param name of the item (FireBoots)
+	 */
+	public FireBoots(String name) {
+		super(name);
+	}
+}
---
+++ Flippers.java
@@ -0,0 +1,14 @@
+/**
+ * Class of the Flippers that the user picks up to be able to walk through water
+ * @author Nathan
+ * @version 1.0
+ */
+public class Flippers extends Item {
+
+	/**
+	 * @param name of the item (FireBoots)
+	 */
+	public Flippers(String name) {
+		super(name);
+	}
+}
