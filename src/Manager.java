f253aba0911af6f1f234389c82b8f4e1b9fa7ea0
Lewis <Lewis@192.168.0.13>
2019-11-28 18:14:47 +0000

    Added scene change from title to game, not ideal!

--- src/Manager.java
+++ src/Manager.java
@@ -1,7 +1,9 @@
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import sprites.*;
 import views.*;
 
@@ -25,12 +27,12 @@ public class Manager {
 		// Load images
 		player = new Image("sprites/player.png");
 		dirt = new Image("sprites/dirt.png");
-		
+		//gameCanvas.getScene().addEventFilter(KeyEvent.KEY_PRESSED, event -> processKeyEvent(event));
 		drawGame();
 	}
 	
-	public Manager() {
-		//null
+	public void start(Scene scene) {
+		scene.addEventFilter(KeyEvent.KEY_PRESSED, event -> processKeyEvent(event));
 	}
 	
 	/**
@@ -46,11 +48,33 @@ public class Manager {
 		// Draw row of dirt images
 		// We multiply by the cell width and height to turn a coordinate in our grid into a pixel coordinate.
 		// We draw the row at y value 2.
+		
 		for (int x = 0; x < 5; x++) {
-			gc.drawImage(dirt, x * GRID_CELL_WIDTH, 2 * GRID_CELL_HEIGHT);	
+			for (int y = 0; y < 5; y++) {
+				gc.drawImage(dirt, x * GRID_CELL_WIDTH, y * GRID_CELL_HEIGHT);	
+			}
 		}
 		
 		// Draw player at current location
-		gc.drawImage(player, playerX * GRID_CELL_WIDTH, playerY * GRID_CELL_HEIGHT);			
+		gc.drawImage(player, playerX * GRID_CELL_WIDTH, playerY * GRID_CELL_HEIGHT);
+	}
+	
+	public void processKeyEvent(KeyEvent event) {
+		switch (event.getCode()) {
+			
+		    case RIGHT:
+		    	// Right key was pressed. So move the player right by one cell.
+	        	playerX = playerX + 1;
+	        	break;		        
+	        default:
+	        	// Do nothing
+	        	break;
+		}
+		
+		// Redraw game as the player may have moved.
+		drawGame();
+		
+		// Consume the event. This means we mark it as dealt with. This stops other GUI nodes (buttons etc) responding to it.
+		event.consume();
 	}
 }
--- src/TitleScreenController.java
+++ src/TitleScreenController.java
@@ -1,8 +1,14 @@
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 public class TitleScreenController {
 
@@ -16,7 +22,16 @@ public class TitleScreenController {
 	
 	@FXML
 	public void startClick(ActionEvent event) throws IOException{
-
+		//loads new stage by swapping root
+		Parent root;
+		Stage stage = new Stage();
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("views/Game.fxml"));
+		root = (Parent)loader.load();
+		Manager controller = (Manager)loader.getController();		
+		Scene scene = new Scene(root, 1920, 1080);
+		controller.start(scene);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
 	
