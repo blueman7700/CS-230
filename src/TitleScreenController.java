3077cdc09b7633cede6dd9893f5c6d55a5fe61a4
Lewis <Lewis@192.168.0.13>
2019-11-28 18:42:28 +0000

    Changed method of scene swap so they use the same window

--- src/TitleScreenController.java
+++ src/TitleScreenController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -24,7 +25,7 @@ public class TitleScreenController {
 	public void startClick(ActionEvent event) throws IOException{
 		//loads new stage by swapping root
 		Parent root;
-		Stage stage = new Stage();
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("views/Game.fxml"));
 		root = (Parent)loader.load();
 		Manager controller = (Manager)loader.getController();		
