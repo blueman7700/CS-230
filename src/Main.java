7b18dd1c101c9e28752676ae759ad95b92513270
Lewis <Lewis@192.168.0.13>
2019-11-28 16:01:41 +0000

    Added a title screen and controllers

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/Game.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/TitleScreen.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1920, 1080));
         primaryStage.show();
     }
-
+    
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/TitleScreenController.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TitleScreenController {
+
+	@FXML
+	Button startButton;
+	
+	@FXML
+	public void initialize() {
+		
+	}
+	
+	@FXML
+	public void startClick(ActionEvent event) throws IOException{
+
+	}
+	
+	
+	
+}
