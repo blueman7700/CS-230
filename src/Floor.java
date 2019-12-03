9b45b1726606e6b3e5c47ae979775f539f46f5ae
Lewis <Lewis@192.168.0.13>
2019-12-03 17:13:29 +0000

    Added sprite file paths

--- src/Floor.java
+++ src/Floor.java
@@ -2,11 +2,17 @@
 public class Floor extends Tile {
 	Item contents;
 	
+	private static String sprite = "sprites/floor.png";
+	
 	public Item getContent() {
 		return contents;
 	}
 	
 	public void setContent(Item i) {
 		contents = i;
+	}
+
+	private static String getSprite() {
+		return sprite;
 	}
 }
--- src/Water.java
+++ src/Water.java
@@ -1,4 +1,10 @@
 
 public class Water extends Tile {
 
+	private static String sprite = "sprites/water.png";
+
+	private static String getSprite() {
+		return sprite;
+	}
+	
 }
