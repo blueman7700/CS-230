77f7091fe89a7720c018f4a3f8ce21f63bc6f45d
Lewis <Lewis@192.168.0.13>
2019-12-03 17:14:46 +0000

    added sprite

--- src/Tile.java
+++ src/Tile.java
@@ -3,9 +3,14 @@ public class Tile {
     private boolean walkable;
 
     //TODO: implement tile superclass.
+    private static String sprite;
 
     public boolean getWalkable() {
 
         return true;
     }
+
+	private static String getSprite() {
+		return sprite;
+	}
 }
