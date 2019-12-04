d8f5ed5181c8001ba75342411be7916863dc84e3
Oliver Morris <blueman7700@gmail.com>
2019-12-04 13:33:06 +0000

    Added an override to the getWalkable method. Now only returns true if it does not contain an item

--- src/Floor.java
+++ src/Floor.java
@@ -1,12 +1,35 @@
 
 public class Floor extends Tile {
 	Item contents;
+
+	public Floor () {
+
+		contents = null;
+	}
 	
 	public Item getContent() {
 		return contents;
 	}
-	
+
+	/**
+	 * set the content of the floor tile.
+	 *
+	 * @param i item that the floor will contain.
+	 */
+
 	public void setContent(Item i) {
 		contents = i;
+	}
+
+	/**
+	 * check if the tile can be moved to by the AI.
+	 *
+	 * @return walkable.
+	 */
+
+	@Override
+	public boolean getWalkable() {
+
+		return (this.contents == null);
 	}
 }
