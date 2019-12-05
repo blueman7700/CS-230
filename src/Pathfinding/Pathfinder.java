197a40f97c088659fb51355681d31d023dca5f0a
Oliver Morris <blueman7700@gmail.com>
2019-12-05 23:48:20 +0000

    Added javadoc to pathfinder interface

--- src/Pathfinding/Pathfinder.java
+++ src/Pathfinding/Pathfinder.java
@@ -1,7 +1,32 @@
 package Pathfinding;
 
+/**
+ * <b>Name: </b>Pathfinder.java
+ * <br>
+ * <p>
+ * Pathfinding interface.
+ * </p>
+ * <br><b>Created:</b> 05/11/2019
+ * <br><b>Last Modified:</b> 05/12/2019
+ * <br> - no copyright
+ * <hr>
+ *
+ * @author Oliver Morris
+ * @version 1.0
+ */
+
 public interface Pathfinder {
 
-	public Path getPath(int startX, int startY, int finX, int finY);
+	/**
+	 * creates a path from start coordinates to final coordinates.
+	 *
+	 * @param startX	starting x coordinate.
+	 * @param startY	starting y coordinate.
+	 * @param finX 		final x coordinate.
+	 * @param finY		final y coordinate.
+	 * @return			path from start to finish.
+	 */
+
+	Path getPath(int startX, int startY, int finX, int finY);
 
 }
