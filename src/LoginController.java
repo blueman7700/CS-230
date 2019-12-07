cee2a728464d0bb5f897e31604692bda895053e6
Oliver Morris <blueman7700@gmail.com>
2019-12-07 14:56:07 +0000

    updated login controller.

--- src/LoginController.java
+++ src/LoginController.java
@@ -1,6 +1,10 @@
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -15,6 +19,10 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+/**
+ * @author Nathan Hardy
+ * @version 1.1
+ */
 public class LoginController {
 
 	@FXML
@@ -25,15 +33,19 @@ public class LoginController {
 	Button deleteBtn;
 	@FXML
 	ListView<String> namesList;
-	
-	
+	final static String FILE_PATH = "src/Files/Users.txt";
+	static String CURR_NAME = "";
+	static String FINAL_NAME ="";
+
 	public LoginController() {};
-	
+
+	/**
+	 * initialize the login controller
+	 */
 	@FXML
-    public void initialize() {
-		
+	public void initialize() {
 		try {
-			ArrayList<String> names = Login.getUsers();
+			ArrayList<String> names = getUsers();
 			ObservableList<String> items =FXCollections.observableArrayList (names);
 			namesList.setItems(items);
 			namesList.setPrefWidth(500);
@@ -41,39 +53,142 @@ public class LoginController {
 		} catch (FileNotFoundException e2) {
 			System.out.println("failed to display names");
 		}
-		
-		
-		
-    }
-	
+	}
+
+	/**
+	 * process login button click.
+	 *
+	 * @param e action event.
+	 * @throws IOException
+	 */
 	@FXML
 	public void loginClick(ActionEvent e) throws IOException {
-		String user = textIn.getText();
-		
-		if(!user.equals("")) {
-			try {
-				Login.loginTwo(user);
-				//loads new stage by swapping root
-		        Parent root;
-		        Stage stage = (Stage)((Node) e.getSource()).getScene().getWindow();
-		        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/Menu.fxml"));
-		        root = (Parent)loader.load();
-		        MenuController controller = (MenuController)loader.getController();
-		        Scene scene = new Scene(root, 1000, 1000);
-		        controller.start(user);
-		        stage.setScene(scene);
-		        stage.show();
-			} catch (Exception e1) {
-				e1.printStackTrace();
+		try {
+			if(!textIn.getText().contains(" ")) {
+				FINAL_NAME = login(textIn.getText());
+			}else {
+				System.out.println("nope");
 			}
+		} catch (Exception e1) {
+
 		}
-		
-		
+		if(FINAL_NAME.length() > 0) {
+			//loads new stage by swapping root
+			Parent root;
+			Stage stage = (Stage)((Node) e.getSource()).getScene().getWindow();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("views/Menu.fxml"));
+			root = (Parent)loader.load();
+			MenuController controller = (MenuController)loader.getController();
+			Scene scene = new Scene(root, 1000, 1000);
+			controller.start(FINAL_NAME);
+			stage.setScene(scene);
+			stage.show();
+		}
+
 	}
-	
+
+	/**
+	 * process delete action.
+	 *
+	 * @param e action event.
+	 */
 	@FXML
 	public void deleteClick(ActionEvent e) {
-		
+		try {
+			deleteLine(textIn.getText());
+			initialize();
+		} catch (Exception e1) {
+
+		}
 	}
-	
+
+
+	/**
+	 * get the users from the user file.
+	 *
+	 * @return users
+	 * @throws FileNotFoundException
+	 */
+	private static ArrayList<String> getUsers() throws FileNotFoundException {
+		ArrayList<String> names = new ArrayList<String>();
+		File file = new File(FILE_PATH);
+		Scanner in = new Scanner(file);
+		while(in.hasNextLine()) {
+			CURR_NAME = in.next();
+			names.add("User: " + CURR_NAME + " , Highest Level: " + in.nextInt());
+		}
+		in.close();
+		return names;
+	}
+
+	/**
+	 * start the game as a desired user or create a new user if it does not exist.
+	 *
+	 * @param name user name.
+	 * @return user name and details
+	 * @throws Exception
+	 */
+	private static String login(String name) throws Exception {
+		ArrayList<String> names = getNames();
+		String output = "";
+		for(String i : names) {
+			if(i.equals(name)) {
+				output = name;
+			}
+		}
+		if(output.equals("") && !name.equals("")) {
+			output = name;
+			String content = name+ " 0";
+			BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH,true));
+			writer.newLine();
+			writer.write(content);
+			writer.close();
+		}
+
+		return output;
+	}
+
+	/**
+	 * Get all the users from the user file.
+	 *
+	 * @return all user names.
+	 * @throws FileNotFoundException if file does not exist then exception is thrown.
+	 */
+	private static ArrayList<String> getNames() throws FileNotFoundException{
+		ArrayList<String> names = new ArrayList<String>();
+		File file = new File(FILE_PATH);
+		Scanner in = new Scanner(file);
+		while(in.hasNextLine()) {
+			CURR_NAME = in.next();
+			names.add(CURR_NAME);
+
+		}
+		in.close();
+		return names;
+	}
+
+	/**
+	 * delete a user from the file.
+	 *
+	 * @param phrase user to delete
+	 * @throws Exception
+	 */
+	private static void deleteLine(String phrase) throws Exception  {
+		File file = new File(FILE_PATH);
+		Scanner in = new Scanner(file);
+		String content = "";
+		while(in.hasNextLine()) {
+			CURR_NAME = in.next();
+
+			if(!(CURR_NAME.equals(phrase))) {
+				content += CURR_NAME + " " + in.nextInt() +"\n";
+			}else {
+				in.nextInt();
+			}
+		}
+		content = content.substring(0, content.length()-1);
+		BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH,false));
+		writer.write(content);
+		writer.close();
+	}
 }
