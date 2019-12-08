d5f329e3e5a175a36078a1b31a4795ea8b4d4397
Lewis <Lewis@192.168.0.13>
2019-12-08 15:52:24 +0000

    Fixed crash when next level doesn't exists

--- src/WinController.java
+++ src/WinController.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -54,29 +57,27 @@ public class WinController {
 	@FXML
 	public void nextClick(ActionEvent e) {
 		//gets the number of the current level and adds one to get the next level
-		System.out.println(level);
 		level = level.substring(level.length()-5, level.length()-4);;
 		int iLevel = Integer.parseInt(level);
 		iLevel++;
-		System.out.println(iLevel);
-		//loads new stage by swapping root
-        Parent root;
-        Stage stage = (Stage)((Node) e.getSource()).getScene().getWindow();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/Game.fxml"));
-        loader.setController(new Manager("Level"+iLevel, user));
-        try {
-			root = (Parent)loader.load();
+
+		try {
+			//Uses scanner to test if the next level exists
+			Scanner test = new Scanner(new File("src/Files/Level"+iLevel+".txt"));
+			//loads new stage by swapping root
+	        Parent root;
+	        Stage stage = (Stage)((Node) e.getSource()).getScene().getWindow();
+	        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/Game.fxml"));
+	        loader.setController(new Manager("Level"+iLevel, user));
+	        root = (Parent)loader.load();
 			Manager controller = loader.getController();  
 			Scene scene = new Scene(root, 1000, 1000);     
 			controller.start(scene);        
 			stage.setScene(scene);
-			stage.show();	
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			System.out.println("Cant load level"+iLevel);
+			stage.show();
+		} catch (IOException e2) {
+			System.out.println("Cant find next level");
 		}
-        
 	}
 	
 	/**
